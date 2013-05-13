a7a8c66ea5fb035d31f3b8a6bf8d6c587837fe2d
ericjuvetartefact <eric@artefactgroup.com>
2013-05-13 16:50:22 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+﻿
+(function ($) {
+
+  var Book = Backbone.Model.extend({
+    defaults:{
+      coverImage:"img/placeholder.png",
+      title:"Some title",
+      author:"Jone Doe",
+      releaseDate:"2012",
+      keywords:"Javascript programming"
+    }
+  })
+  
+})(jQuery);
